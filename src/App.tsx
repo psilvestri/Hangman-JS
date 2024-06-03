3f2cf2060199acca4df886d0481edff04bf2e719
peters <petes726@gmail.com>
2024-06-03 10:58:38 -0400

    Updating guessed letter to callback function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import words from './wordList.json';
 import { HangmanDrawing } from './HangmanDrawing';
 import { HangmanWord } from './HangmanWord';
@@ -15,19 +15,22 @@ function App() {
 		(letter) => !wordToGuess.includes(letter)
 	);
 
-	function addGuessdLetter(letter: string) {
-		if (guessedLetters.includes(letter)) return;
+	const addGuessedLetter = useCallback(
+		(letter: string) => {
+			if (guessedLetters.includes(letter)) return;
 
-		setGuessedLetters((currentLetters) => [...currentLetters, letter]);
-	}
+			setGuessedLetters((currentLetters) => [...currentLetters, letter]);
+		},
+		[guessedLetters]
+	);
 
 	useEffect(() => {
 		const handler = (e: KeyboardEvent) => {
 			const key = e.key;
 			if (!key.match(/^[a-z]$/)) return;
-      
+
 			e.preventDefault();
-			addGuessdLetter(key);
+			addGuessedLetter(key);
 		};
 
 		document.addEventListener('keypress', handler);
